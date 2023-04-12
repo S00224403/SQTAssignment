6c532d95397766db8df1aaad6206563b20b27eeb
MateuszWeclawowicz <S00224403@mail.itsligo.ie>
2023-04-12 14:46:27 +0100

    decoupled and refactored code with interface

--- SQTAssignment/InsuranceService.cs
+++ SQTAssignment/InsuranceService.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace SQTAssignment
 {
+    interface IDiscountService
+    {
+        double GetDiscount();
+    }
+
     class InsuranceService
     {
+        private readonly IDiscountService _discountService;
+
+        public InsuranceService(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+
         public double CalcPremium(int age, string location)
         {
             double premium;
             if (location == "rural")
             {
-                if ((age >= 18) && (age < 30))
+                if (age >= 18 && age < 30)
                     premium = 5.0;
                 else if (age >= 31)
                     premium = 2.50;
@@ -22,7 +34,7 @@ namespace SQTAssignment
             }
             else if (location == "urban")
             {
-                if ((age >= 18) && (age <= 35))
+                if (age >= 18 && age <= 35)
                     premium = 6.0;
                 else if (age >= 36)
                     premium = 5.0;
@@ -34,14 +46,12 @@ namespace SQTAssignment
                 premium = 0.0;
             }
 
-            DiscountService ds = new DiscountService();
-            double discount = ds.GetDiscount();
+            double discount = _discountService.GetDiscount();
 
             if (age >= 50)
-                premium = premium * discount;
+                premium *= discount;
 
             return premium;
         }
     }
-
 }
