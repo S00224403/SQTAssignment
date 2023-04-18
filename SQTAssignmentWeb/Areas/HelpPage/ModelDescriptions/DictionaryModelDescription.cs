ac9f1e0340e9483ce3c14ee582d97422f398a159
MateuszWeclawowicz <98536909+MateuszWeclawowicz@users.noreply.github.com>
2023-04-18 22:16:01 +0100

    Delete SQTAssignmentWeb directory

--- SQTAssignmentWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SQTAssignmentWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SQTAssignmentWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SQTAssignmentWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
