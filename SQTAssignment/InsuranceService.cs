77c7240d3e1bfde08fc52a276cd03671a7a0080d
MateuszWeclawowicz <S00224403@mail.itsligo.ie>
2023-04-18 22:15:31 +0100

    created web form with working calcpremium

--- SQTAssignment/InsuranceService.cs
+++ SQTAssignment/InsuranceService.cs
@@ -18,6 +18,10 @@ namespace SQTAssignment
     public class InsuranceService
     {
         private readonly IDiscountService _discountService;
+        public InsuranceService()
+        {
+
+        }
 
         public InsuranceService(IDiscountService discountService)
         {
@@ -51,6 +55,39 @@ namespace SQTAssignment
             }
 
             double discount = _discountService.GetDiscount();
+
+            if (age >= 50)
+                premium *= discount;
+
+            return premium;
+        }
+        public double CalculatePremium(int age, string location)
+        {
+            double premium;
+            if (location == "rural")
+            {
+                if (age >= 18 && age <= 30)
+                    premium = 5.0;
+                else if (age >= 31)
+                    premium = 2.50;
+                else
+                    premium = 0.0;
+            }
+            else if (location == "urban")
+            {
+                if (age >= 18 && age <= 35)
+                    premium = 6.0;
+                else if (age >= 36)
+                    premium = 5.0;
+                else
+                    premium = 0.0;
+            }
+            else
+            {
+                premium = 0.0;
+            }
+
+            double discount = 0.5;
 
             if (age >= 50)
                 premium *= discount;
---
+++ SQTAssignmentWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SQTAssignment;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public int Age { get; set; }
+
+        [BindProperty]
+        public string Location { get; set; }
+
+        public double Premium { get; set; }
+
+        public void OnPost()
+        {
+            // Call InsuranceService to calculate premium
+            InsuranceService insuranceService = new InsuranceService();
+            Premium = insuranceService.CalculatePremium(Age, Location);
+        }
+    }
+}
